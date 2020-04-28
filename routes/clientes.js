c4ee52c884a7a6d68df5315f4b92426937a5929f
Santiago Mena <santiagomenape@gmail.com>
2020-04-28 20:55:20 -0300

    Enviar comandos al cliente API

--- routes/clientes.js
+++ routes/clientes.js
@@ -2,12 +2,29 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const router = express.Router();
+const { io } = require('../bin/www');
 
-/* GET home page. */
+/**
+ * Obtener todos los clientes activos
+ */
 router.get('/', (req, res, next) => {
   let clientes = fs.readFileSync(path.join(__dirname, '../dbs/clientes.json'), 'utf8');
   
   res.send( clientes );
+  next();
 });
+
+/**
+ * Envíar comando al cliente
+ */
+router.post('/:id', (req, res) => {
+  const { id } = req.params;
+  const { body: comando } = req;
+
+  req.app.io.to( id ).emit('comando', comando);
+
+  return res.json('ok');
+});
+
 
 module.exports = router;
